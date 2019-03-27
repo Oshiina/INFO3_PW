6f710aaa7bc721d3430e9ea9ecf83d641d2f5c9a
Oshiina <nicolaslabbe2000@gmail.com>
2019-03-27 16:07:50 +0100

    Commit #3 TP1 fini

---
+++ src/app/details/details.page.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Movie } from '../home/home.page';
+import { moveEmbeddedView } from '@angular/core/src/view';
+
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.page.html',
+  styleUrls: ['./details.page.scss'],
+})
+export class DetailsPage implements OnInit {
+
+  movie: Movie;
+
+  constructor(private readonly router: Router) {
+    this.movie = this.router.getCurrentNavigation().extras.state.movie;
+  }
+
+
+
+  ngOnInit() {
+  }
+
+
+  buttonBack() {
+    this.router.navigate(['/home']);
+  }
+}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
-interface Movie {
+export interface Movie {
   title: string;
   id: string;
 }
@@ -15,10 +16,28 @@ export class HomePage {
 
   movies: Movie[] = [];
 
-  constructor(private readonly router: Router) {
+  constructor(
+    private readonly router: Router,
+    public alertController: AlertController
+    ) {
   }
 
   getMovie(search: string): void {
-    this.movies = search.length > 2 ? [{title: 'search', id: '1'}] : [];
+    this.movies = search.length > 2 ? [{title: search, id: '1'}] : [];
   }
+
+  showDetails(movie: Movie): void {
+    this.router.navigate(['/details'], { state: {movie} });
+  }
+
+  async buttonAlerte() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: 'This is an alert message.',
+      buttons: ['OK']
+    });
+    return await alert.present();
+  }
+
 }
